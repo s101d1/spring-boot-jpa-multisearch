5cd88e66c19c5dc7c9ad9e59e566652527a44175
s101d1 <s101d1@users.noreply.github.com>
2018-08-18 07:48:39 +0700

    Add UserControllerTest

---
+++ src/test/java/com/example/UserControllerTest.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.example.controllers.UserController;
+import com.example.repositories.UserRepository;
+import com.querydsl.core.BooleanBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Arrays;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserControllerTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Test
+    public void testSearchUser() {
+        ArgumentCaptor<BooleanBuilder> argumentCaptor = ArgumentCaptor.forClass(BooleanBuilder.class);
+
+        when(userRepository.findAll(argumentCaptor.capture(), any(Pageable.class))).thenReturn(mock(Page.class));
+
+        userController.searchUser(Arrays.asList("john,bob", "doe"), mock(Pageable.class));
+
+        verify(userRepository).findAll(any(BooleanBuilder.class), any(Pageable.class));
+
+        BooleanBuilder booleanBuilder = argumentCaptor.getValue();
+        assertEquals("lower(user.name) in [john, bob] || lower(user.name) like %doe%", booleanBuilder.getValue().toString());
+    }
+}
